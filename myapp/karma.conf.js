f6e88b00cdbf26e783af33f4d276dce90f18217a
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-01-06 10:00:17 +0100

    Add ng test support

--- myapp/karma.conf.js
+++ myapp/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const isDocker = require('is-docker')();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -27,7 +29,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+
+    // cf.: https://hackernoon.com/running-karma-tests-with-headless-chrome-inside-docker-ae4aceb06ed3
+    browsers: isDocker ? ['ChromeDocker'] : ['Chrome'],
+    customLaunchers: {
+      ChromeDocker: {
+        base: 'ChromeHeadless',
+        flags: ['--headless', '--disable-gpu', '--no-sandbox', '--remote-debugging-port=9222']
+      }
+    },
   });
 };
